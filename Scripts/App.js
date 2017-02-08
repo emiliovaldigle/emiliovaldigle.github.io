6270cab74563c9f54f139cc1c0ad1f4bc8943001
Emilio <emiliovaldigle@gmail.com>
2017-02-08 18:12:48 -0300

    Attemp of fixing bug

--- Scripts/App.js
+++ Scripts/App.js
@@ -1,3 +1,9 @@
 var App = angular.module('App', ['ngMaterial']);
 
 App.controller('MainController', MainController);
+
+App.config([
+  '$interpolateProvider', function($interpolateProvider) {
+    return $interpolateProvider.startSymbol('{(').endSymbol(')}');
+  }
+]);
