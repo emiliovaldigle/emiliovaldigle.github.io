311c9968a98d46b639f98b1c103b826043f54d17
Emilio <emiliovaldigle@gmail.com>
2017-03-11 14:09:30 -0300

    Testing ngRoute

--- Controller/MainController.js
+++ Controller/MainController.js
@@ -1,5 +1,31 @@
-var App = angular.module("App",['ngMaterial']);
+var App = angular.module("App",['ngMaterial', 'ngRoute']);
 
-App.controller("MainController",['$scope',function($scope){
+App.controller("MainController",['$scope',function($scope) {
    $scope.title = "My Landing Page"; 
+   $scope.menu = [
+       {menuitem: 'Inicio', icon: 'dashboard', path: 'index'},
+       {menuitem: 'Biografía', icon: 'account_balance', path: 'biography'},
+       {menuitem: 'Clientes' ,icon: 'contacts', path:'clients'},
+       {menuitem: 'Contacto' ,icon: 'perm_phone_msg', path:'contact'},
+       {menuitem: 'Artículos', icon: 'class', path: 'articles'}
+      ];
 }]);
+
+App.config(function($routeProvider){
+   $routeProvider
+   .when("/index",{
+     templateUrl : "Views/index.html"  
+   })
+   .when("/biography",{
+     templateUrl : "Views/biography.html"  
+   })
+   .when("/clients",{
+     templateUrl : "clients.html"  
+   })
+   .when("/contact",{
+     templateUrl : "contact.html"  
+   })
+   .when("/articles",{
+     templateUrl : "articles.html"  
+   });
+});
